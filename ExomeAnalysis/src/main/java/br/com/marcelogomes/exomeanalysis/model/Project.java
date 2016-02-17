822a72ce0820ec6f461df9d8ad884e63caa88ee5
marcelo <marcelo@DESKTOP-BFECFRE>
2016-02-17 19:31:26 -0200

    maked the manager profile

---
+++ ExomeAnalysis/src/main/java/br/com/marcelogomes/exomeanalysis/model/Project.java
@@ -0,0 +1,65 @@
+package br.com.marcelogomes.exomeanalysis.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author marcelo
+ */
+@Entity
+public class Project implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Project)) {
+            return false;
+        }
+        Project other = (Project) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.marcelogomes.exomeanalysis.model.Project[ id=" + id + " ]";
+    }
+    
+}
